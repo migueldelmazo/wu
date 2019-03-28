958de9931513461814f72186a7db5be6e112e18b
Miguel del Mazo <info@migueldelmazo.com>
2019-03-28 22:09:02 +0100

    Added _.parseCamelCaseDeep

--- src/libs/lodash/parser.js
+++ src/libs/lodash/parser.js
@@ -1,6 +1,10 @@
 import _ from 'lodash'
 
 _.mixin({
+  
+  parseCamelCaseDeep: (data) => {
+    return _.mapDeep(data, _.camelCase)
+  },
 
   parseArray: (arr) => {
     return _.isArray(arr) ? arr : [arr]
