7a85aba0c6b9823ee04bce87806ad8eba417b9c4
Miguel del Mazo <info@migueldelmazo.com>
2019-04-11 18:29:35 +0200

    Trivial changes

--- src/libs/lodash/functions.js
+++ src/libs/lodash/functions.js
@@ -2,10 +2,6 @@ import _ from 'lodash'
 
 _.mixin({
 
-  fnRun: (fn, ...args) => {
-    return fn(...args)
-  },
-
   fnsRun: (fns, ...args) => {
     _.each(_.parseArray(fns), (fn) => fn(...args))
   }
--- src/libs/lodash/utils.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash'
-
-_.mixin({
-
-  concatArgs: (args, arr) => {
-    return args.concat(arr)
-  }
-
-})
