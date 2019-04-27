6715efb0e79c3199643e0768bdb8914dfa500ad3
Miguel del Mazo <info@migueldelmazo.com>
2019-04-27 18:05:37 +0200

    Added notFound functionality to Wu Router

--- src/libs/wu/router.js
+++ src/libs/wu/router.js
@@ -2,27 +2,41 @@ import _ from 'lodash'
 import { wu, setInModel } from './common'
 import { getDefinitions, setDefinition } from './definition'
 
+// set app.router.location and set app.router.notFound
+
 const updateModel = () => {
-  wu.model.set('app.router', _.getWindowLocationData())
+  wu.model.set('app.router', {
+    location: _.getWindowLocationData(),
+    notFound: !routerExists()
+  })
 }
 
+const routerExists = () => {
+  const location = _.getWindowLocationData()
+  return _.some(getDefinitions('router'), (definition) => {
+    return _.matchRouteParams(location.pathName, definition.urlPattern)
+  })
+}
+
+// ensure routers
+
 const run = () => {
-  const currentRouter = wu.model.get('app.router')
-  const found = _.some(getDefinitions('router'), (definition, name) => {
-    const result = getRouteMatches(definition, currentRouter)
+  const router = wu.model.get('app.router.location')
+  _.each(getDefinitions('router'), (definition, name) => {
+    const result = {
+      isActive: _.matchRouteParams(router.pathName, definition.urlPattern),
+      params: _.getRouteParams(router.pathName, definition.urlPattern)
+    }
     _.consoleGroup('router', 'Router: set ' + name, 'Result:', result)
     setInModel(definition, result)
     _.consoleGroupEnd()
-    return result.isActive
   })
-  wu.model.set('app.router.found', found)
 }
 
-const getRouteMatches = (definition, router) => {
-  return {
-    isActive: _.matchRouteParams(router.pathName, definition.urlPattern),
-    params: _.getRouteParams(router.pathName, definition.urlPattern)
-  }
+const watchRouter = () => {
+  wu.model.watch('app.router.location', run, undefined, {
+    type: 'ensurer'
+  })
 }
 
 export default {
@@ -33,13 +47,11 @@ export default {
       update: true
     })
   },
-  
+
   start: () => {
     _.initRouter(updateModel)
     updateModel()
-    wu.model.watch('app.router.url', run, undefined, {
-      type: 'ensurer'
-    })
+    watchRouter()
   }
 
 }
