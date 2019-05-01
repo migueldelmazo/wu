22e6c11a5eb8f4aaf3a13c2bd39a073ad6813f47
Miguel del Mazo <info@migueldelmazo.com>
2019-05-01 15:50:05 +0200

    Removed app and login example code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDom from 'react-dom'
 import wu from './libs/wu'
-import AppView from './app/AppView'
-import './user/login'
-import './user/profile'
 
-wu.start()
-
-ReactDom.render(<AppView />, document.getElementById('root'))
+export default wu
