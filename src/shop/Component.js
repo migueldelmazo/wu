944b5767fe1e8dbd95739f4886ce535c093b2179
Miguel del Mazo <migueldelmazo@gmail.com>
2023-05-16 14:03:00 +0200

    Trivial changes

--- src/shop/Component.js
+++ src/shop/Component.js
@@ -1,21 +1,80 @@
-import '.'
+import { addProductToCart, getCartProducts } from '.'
 import { useWuGet } from '../wu'
 
 const Shop = () => {
   const isLoading = useWuGet('shop.requests.products.isLoading')
   const products = useWuGet('shop.products')
+  const totalCartPrice = useWuGet('shop.totalCartPrice')
+  const totalCartUnits = useWuGet('shop.totalCartUnits')
+  const cartShippingCosts = useWuGet('shop.cartShippingCosts')
+  const cartProducts = useWuGet(getCartProducts)
 
   return (
     <>
       <h2 className="title is-4">Shop</h2>
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-5">Products</h2>
 
-      {isLoading && 'Loading products...'}
+          {isLoading && 'Loading products...'}
 
-      {products.map(product => (
-        <li key={product.id}>
-          {product.name}: {product.price} &euro;
-        </li>
-      ))}
+          <ul>
+            {products.map(product => (
+              <li key={product.id}>
+                {product.name}: {product.price} &euro;{' '}
+                <button
+                  className="button is-small is-info mb-1"
+                  onClick={() => addProductToCart(product.id)}
+                >
+                  Add
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="column">
+          <h2 className="title is-5">Cart</h2>
+
+          {isLoading && 'Loading products...'}
+
+          <table className="table is-bordered is-striped">
+            <thead className="has-background-info">
+              <tr>
+                <th className="has-text-white">Name</th>
+                <th className="has-text-white">Price</th>
+                <th className="has-text-white">Units</th>
+                <th className="has-text-white">Total price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartProducts.map(cartProduct => (
+                <tr key={cartProduct.id}>
+                  <td>{cartProduct.name}</td>
+                  <td>{cartProduct.price} &euro;</td>
+                  <td>{cartProduct.units}</td>
+                  <td>{cartProduct.totalPrice} &euro;</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot className="has-text-white has-background-info">
+              <tr>
+                <th className="has-text-white" colSpan={2}>
+                  Total
+                </th>
+                <th className="has-text-white">{totalCartUnits}</th>
+                <th className="has-text-white">{totalCartPrice} &euro;</th>
+              </tr>
+              <tr>
+                <th className="has-text-white" colSpan={3}>
+                  Shipping costs
+                </th>
+                <th className="has-text-white">{cartShippingCosts} &euro;</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
 
       <hr />
     </>
--- src/shop/index.js
+++ src/shop/index.js
@@ -1,6 +1,72 @@
-import { wuReact, wuReactiveFetch } from '../wu/'
+import { wuGet, wuReact, wuReactiveFetch, wuSet } from '../wu/'
+
+// wu set
+
+export const addProductToCart = wuSet(
+  (cartProducts, productId) => {
+    const cartProduct = cartProducts.find(({ id }) => id === productId)
+    if (cartProduct) {
+      cartProduct.units++
+    } else {
+      cartProducts.push({
+        id: productId,
+        units: 1
+      })
+    }
+    return { 'shop.cartProducts': cartProducts }
+  },
+  ['shop.cartProducts']
+)
+
+// wu get
+
+export const getCartProducts = wuGet(
+  (products, cartProducts) => {
+    return cartProducts.map(cartProduct => {
+      const product = products.find(({ id }) => cartProduct.id === id)
+      cartProduct.name = product.name
+      cartProduct.price = product.price
+      cartProduct.totalPrice = product.price * cartProduct.units
+      return cartProduct
+    })
+  },
+  ['shop.products', 'shop.cartProducts']
+)
+
+// wu react
 
 wuReact(() => ({ 'shop.products': [] }), ['wu.isReady'])
+wuReact(() => ({ 'shop.cartProducts': [] }), ['wu.isReady'])
+
+wuReact(
+  cartProducts => {
+    const totalCartPrice = cartProducts.reduce((total, cartProduct) => {
+      return total + cartProduct.totalPrice
+    }, 0)
+    return { 'shop.totalCartPrice': totalCartPrice }
+  },
+  [getCartProducts]
+)
+
+wuReact(
+  cartProducts => {
+    const totalCartUnits = cartProducts.reduce((total, cartProduct) => {
+      return total + cartProduct.units
+    }, 0)
+    return { 'shop.totalCartUnits': totalCartUnits }
+  },
+  [getCartProducts]
+)
+
+wuReact(
+  totalCartPrice => {
+    const cartShippingCosts = totalCartPrice < 100 ? 10 : 0
+    return { 'shop.cartShippingCosts': cartShippingCosts }
+  },
+  ['shop.totalCartPrice']
+)
+
+// wu fetch
 
 wuReactiveFetch(
   userToken => {
